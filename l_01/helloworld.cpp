c916241a0f7001fd144ddfafe403ee9e3854e996
smietanka4 <wloczewskikarol5@gmail.com>
2026-03-19 13:46:30 +0100

    pierwsze styki

---
+++ l_01/helloworld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+
+      // This is a comment 
+
+      /*
+      This
+      is
+      a
+      multi line
+      comment
+      */
+      std::cout << "I like pizza" << '\n';
+      std::cout << "It's really good!" << std::endl;
+      return 0;
+}
---
+++ l_01/variables.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+      int x; // declaration
+      x = 5;
+
+      // double (number including decimal)
+      double price = 10.99;
+
+      // single character
+      char grade = 'A';
+
+      // boolean
+      bool student = false;
+
+      // string
+
+      std::string name = "Karol";
+      std::cout << "Hello " << name << '\n';
+
+      
+
+
+      return 0;
+}
