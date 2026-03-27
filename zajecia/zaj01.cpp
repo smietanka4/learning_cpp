8d22e68409c95b32bc49181b759cc4dd1e788b50
smietanka4 <wloczewskikarol5@gmail.com>
2026-03-27 12:30:16 +0100

    przykladowe zadania z zajec

--- zajecia/zaj01.cpp
+++ zajecia/zaj01.cpp
@@ -6,11 +6,21 @@ using namespace std;
 int zadanie2();
 int zadanie3();
 int zadanie4();
+int zadanie5();
+int zadanie6();
+int zadanie7();
+int zadanie8();
+int zadanie9();
 
 int main() {
     // zadanie2();
     // zadanie3();
-    zadanie4();
+    // zadanie4();
+    // zadanie5();
+    // zadanie6();
+    // zadanie7();
+    // zadanie8();
+    zadanie9();
 
     return 0;
 }
@@ -85,4 +95,83 @@ int zadanie4() {
         cout << "Błąd: Podano liczbę spoza zakresu" << '\n';
         break;
     }
+
+    return 0;
+}
+
+int zadanie5() {
+    int n{};
+    int suma{0};
+
+    cout << "Podaj liczbę: " << '\n';
+    cin >> n;
+    
+    for (int i = 0; i <= n; i++) {
+        suma += i;
+    }
+
+    cout << "Suma jest równa: " << suma << '\n';
+
+    return 0;
+}
+
+int zadanie6() {
+    string haslo{};
+    string correct_haslo{"cpp123"};
+
+    while (haslo != correct_haslo);
+        cout << "Wprowadź hasło: " << '\n';
+        cin >> haslo;
+        if (haslo != correct_haslo) {
+            cout << "Niepoprawne hasło, spróbuj ponownie" << '\n';
+        } else {
+            cout << "Dostęp przyznany." << '\n';
+        }
+
+        return 0;
+}
+
+int zadanie7() {
+    string slowo{};
+    cout << "Podaj słowo: " << '\n';
+    cin >> slowo;
+    cout << "Długość podanego słowa: " << slowo.length() << '\n';
+
+    return 0;
+}
+
+int zadanie8() {
+    string slowo{};
+    cout << "Podaj słowo: " << '\n';
+    cin >> slowo;
+
+    if (slowo.length() != 0) {
+        cout << "Pierwsza literka podanego słowa: " << slowo[0] << "\nOstatnia literka podanego słowa: " << slowo[slowo.length()-1] << '\n';
+    }
+
+    return 0;
+}
+
+int zadanie9() {
+    string slowo{};
+    cout << "Podaj słowo, które chcesz sprawdzić czy jest palindromem: " << '\n';
+    cin >> slowo;
+
+    bool isPalindrome{true};
+
+    for (int i = 0; i <= slowo.length() / 2; i++) {
+        if (slowo[i] != slowo[slowo.length() - 1 - i]) {
+            isPalindrome = false;
+            cout << "Słowo nie jest palindromem" << '\n';
+            break;
+        } else {
+            cout << slowo[i] << '=' << slowo[slowo.length() - i - 1] << '\n';
+        }
+    }
+
+    if (isPalindrome) {
+            cout << "Słowo jest palindromem" << '\n';
+    }
+
+    return 0;
 }
